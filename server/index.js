31ea91a85f870489ce1e86a2244cdc78704e8c3a
unknown <Eklavya lalwani>
2023-12-09 03:30:27 +0530

    test 3

--- server/index.js
+++ server/index.js
@@ -31,7 +31,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(
+  {
+    origin: ['http://localhost:3000/'],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  }
+));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 aws.config.update({
   accessKeyId: process.env.ACCESS_KEY,
