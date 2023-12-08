9033f03a8ccf91a893cc9327173794859782f08a
eklavya-15 <121112673+eklavya-15@users.noreply.github.com>
2023-12-09 03:46:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,8 +33,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
   {
-    origin: ['http://localhost:3000/'],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    origin: ['https://socio-sphere-six.vercel.app/'],
+    methods: ["GET", "POST"],
   }
 ));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
