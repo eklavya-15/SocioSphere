9f976352ddac240c4b98beac8115563fbb564920
eklavya-15 <121112673+eklavya-15@users.noreply.github.com>
2023-12-09 04:00:36 +0530

    Update index.js 2

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
   {
-    origin: ['https://socio-sphere-six.vercel.app/'],
+    origin: ['/'],
     methods: ["GET", "POST"],
   }
 ));
