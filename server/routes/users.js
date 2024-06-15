e9626ca1d3860f4a2fa77f5aff82f10a62651ac1
eklavya-15 <121112673+eklavya-15@users.noreply.github.com>
2024-06-15 19:20:31 +0530

    Update SuggestionWidget.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getUser,
   getUserFriends,
+  getAllUsers,
   addRemoveFriend,
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
@@ -11,6 +12,7 @@ const router = express.Router();
 /* READ */
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
+router.get("", verifyToken, getAllUsers);
 
 /* UPDATE */
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
