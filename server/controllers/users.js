f93328a0f71602117939a1efaaebf4bf59ff9420
eklavya-15 <121112673+eklavya-15@users.noreply.github.com>
2024-06-18 02:59:27 +0530

    Update users.js 5

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -40,34 +40,32 @@ export const getUserFriends = async (req, res) => {
 };
 
 /* UPDATE */
-export const addRemoveFriend = async (req, res) => {
+export const addRemoveFriend = async (req,res) => {
   try {
-    
-    const { id, friendId } = req.params;
-    const user = await User.findById(id).populate('friends');
+    const {id,friendId} = req.params;
+    const user = await User.findById(id);
     const friend = await User.findById(friendId);
-
-    if (user.friends.includes(friendId)) {
-      user.friends = user.friends.filter((id) => id !== friendId);
-      friend.friends = friend.friends.filter((id) => id !== id);
+    console.log("hi",friend.friends);
+    console.log("hi",user.friends);
+    if (!user.friends.includes(friendId)) {
+      user.friends.push(friendId)
+      friend.friends.push(id)
     } else {
-      user.friends.push(friendId);
-      friend.friends.push(id);
+      user.friends = user.friends.filter(id => id !== friendId)
+      friend.friends = friend.friends.filter(friendId => friendId !== id)
     }
+   
     await user.save();
     await friend.save();
-
-    const friends = await Promise.all(
-      user.friends.map((id) => User.findById(id))
-    );
-    const formattedFriends = friends.map(
+    console.log("how",friend.friends);
+    console.log("how",user.friends);
+    const formattedFriends = user.friends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
         return { _id, firstName, lastName, occupation, location, picturePath };
       }
     );
-
-    res.status(200).json(formattedFriends);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
-  }
-};
+    res.status(200).json(formattedFriends)
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  } 
+}
