756d60002deacf17cffeb9c7b69af04c1400cc93
eklavya-15 <121112673+eklavya-15@users.noreply.github.com>
2024-06-17 19:06:57 +0530

    Adding chat feature part2

--- server/index.js
+++ server/index.js
@@ -39,8 +39,6 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(express.json());
-app.use(express.urlencoded());
 app.use(cors(
   {
     origin: ['/'],
