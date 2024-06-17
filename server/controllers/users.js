c75ea35e54004bf75938786dd70827f1fdf86a19
eklavya-15 <121112673+eklavya-15@users.noreply.github.com>
2024-06-18 03:06:20 +0530

    Update users.js 7

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -59,13 +59,17 @@ export const addRemoveFriend = async (req,res) => {
     await friend.save();
     console.log("how",friend.friends);
     console.log("how",user.friends);
-    const formattedFriends = user.friends.map(
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
         return { _id, firstName, lastName, occupation, location, picturePath };
       }
     );
-    res.status(200).json(formattedFriends)
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  } 
-}
+
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
